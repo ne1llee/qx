f27031d01171b1bb958b706dbd106e676ddc6cc5
ne1llee <130048@qq.com>
2020-10-22 14:30:03 +0800

    fix

--- scripts/quanminxiaoshuo.js
+++ scripts/quanminxiaoshuo.js
@@ -1,9 +1,9 @@
 /* 
 全民小说 unlock vip
 QX : 
-^https?:\/\/my.arstsgc.cn\/v2\/center\/index.api.* url script-response-body https://raw.githubusercontent.com/nzw9314/QuantumultX/master/Script/aimeiju.js
+^https?:\/\/my.arstsgc.cn\/v2\/center\/index.api.* url script-response-body https://raw.githubusercontent.com/ne1llee/qx/main/scripts/quanminxiaoshuo.js
 Surge4：
-http-response ^https?:\/\/mjapp.anlujia.com\/index\.php\/app\/ios\/(vod\/show|(user|vod|topic|type)\/index) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/aimeiju.js
+http-response ^https?:\/\/my.arstsgc.cn\/v2\/center\/index.api.*  requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/ne1llee/qx/main/scripts/quanminxiaoshuo.js
 hostname = my.arstsgc.cn
 */
 var body = $response.body;
