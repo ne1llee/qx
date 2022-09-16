1695781c99c3029697ae4ab346a4a8f2668592d5
ne1llee <12810839+ne1llee@users.noreply.github.com>
2022-09-16 21:55:24 +0800

    add 羊了个羊

---
+++ scripts/ylgy.js
@@ -0,0 +1,12 @@
+var body = $response.body
+var url = $request.url
+
+if (body) {
+  var obj = JSON.parse($response.body)
+  obj.data.map_data =
+    '{ "widthNum": 8, "heightNum": 10, "levelKey": 80001, "blockTypeData": { "1": 2, "4": 1, "13": 2 }, "levelData": { "1": [ { "id": "1-16-16", "type": 0, "rolNum": 16, "rowNum": 16, "layerNum": 1, "moldType": 1, "blockNode": null } ], "2": [ { "id": "1-16-16", "type": 0, "rolNum": 16, "rowNum": 16, "layerNum": 2, "moldType": 2, "blockNode": null } ], "3": [ { "id": "1-16-16", "type": 0, "rolNum": 16, "rowNum": 16, "layerNum": 3, "moldType": 3, "blockNode": null } ] } }'
+
+  $done({ body: JSON.stringify(obj) })
+} else {
+  $done({})
+}
