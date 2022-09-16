fa1479d2d1ee92cbeca7d3e38ee126b4ea219f4c
ne1llee <12810839+ne1llee@users.noreply.github.com>
2022-09-16 22:24:04 +0800

    u

--- scripts/ylgy.js
+++ scripts/ylgy.js
@@ -4,7 +4,7 @@ var url = $request.url
 if (body) {
   var obj = JSON.parse($response.body)
   obj.data.map_data =
-    '{ "widthNum": 1, "heightNum": 1, "levelKey": 80001, "blockTypeData": { "1": 1, "2": 1, "3": 1 }, "levelData": { "1": [ { "id": "1-16-16", "type": 1, "rolNum": 16, "rowNum": 16, "layerNum": 1, "moldType": 1, "blockNode": null } ], "2": [ { "id": "1-16-16", "type": 1, "rolNum": 16, "rowNum": 16, "layerNum": 1, "moldType": 1, "blockNode": null } ], "3": [ { "id": "1-16-16", "type": 1, "rolNum": 16, "rowNum": 16, "layerNum": 1, "moldType": 1, "blockNode": null } ] } }'
+    '{ "widthNum": 8, "heightNum": 10, "levelKey": 80001, "blockTypeData": { "1": 1, "2": 1, "3": 1 }, "levelData": { "1": [ { "id": "1-16-16", "type": 1, "rolNum": 16, "rowNum": 16, "layerNum": 1, "moldType": 1, "blockNode": null } ], "2": [ { "id": "1-16-16", "type": 1, "rolNum": 16, "rowNum": 16, "layerNum": 1, "moldType": 1, "blockNode": null } ], "3": [ { "id": "1-16-16", "type": 1, "rolNum": 16, "rowNum": 16, "layerNum": 1, "moldType": 1, "blockNode": null } ] } }'
 
   $done({ body: JSON.stringify(obj) })
 } else {
